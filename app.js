b10cd9248a9057ddc380755188580a9aeafb0bd6
Brandon <brandon121j@gmail.com>
2021-11-09 21:04:57 -0500

    Addition of jwtMiddleware file and shared folder

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var users = require('./routes/users/usersRouter')
 
 var app = express();
 
