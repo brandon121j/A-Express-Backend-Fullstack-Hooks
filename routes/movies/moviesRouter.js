bd1b9114054a68fa00dace465227255c14dcc515
Brandon <brandon121j@gmail.com>
2021-11-15 13:54:40 -0500

    Changes made to moviesRouter.js file

--- routes/movies/moviesRouter.js
+++ routes/movies/moviesRouter.js
@@ -6,8 +6,13 @@ const {
     getAllFavoriteMovies
 } = require('./controller/moviesController')
 
-const { jwtMiddleware } = require('../shared/jwtMiddleware')
-
+const {
+    jwtMiddleware,
+    emptyValidator,
+    undefinedValidator,
+    createDataValidator,
+    loginDataValidator
+} = require ('../shared/index')
 router.post('/add-favorite', jwtMiddleware, addToFavorites);
 
 router.get('/', jwtMiddleware, getAllFavoriteMovies);
